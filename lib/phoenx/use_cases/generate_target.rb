42c5ed05eaaaf0685509dd7cea15788dc091dc62
Simon Seyer <simon.seyer@gmail.com>
2018-08-06 12:56:37 +0200

    Removed duplicate code

--- lib/phoenx/use_cases/generate_target.rb
+++ lib/phoenx/use_cases/generate_target.rb
@@ -134,31 +134,6 @@ module Phoenx
 			end
 		end
 		
-		def add_headers(header_files, excluded_header_files, attributes)
-			headers = Phoenx.merge_files_array(header_files, excluded_header_files)
-			unless !header_files || header_files.empty? || !headers.empty?
-				puts "No #{attributes["ATTRIBUTES"].first} headers found".yellow
-			end
-			Phoenx.add_groups_for_files(@project, headers)
-			headers.each do |header|
-				file = Phoenx.get_or_add_file(@project,header)
-				build_file = self.target.headers_build_phase.add_file_reference(file, true)
-				build_file.settings = attributes
-			end
-		end
-		
-		def add_public_headers
-			self.add_headers(@target_spec.public_headers, @target_spec.excluded_public_headers, ATTRIBUTES_PUBLIC_HEADERS)
-		end
-		
-		def add_private_headers
-			self.add_headers(@target_spec.private_headers, @target_spec.excluded_private_headers, ATTRIBUTES_PRIVATE_HEADERS)
-		end
-		
-		def add_project_headers
-			self.add_headers(@target_spec.project_headers, @target_spec.excluded_project_headers, ATTRIBUTES_PROJECT_HEADERS)
-		end
-		
 		def add_config_files
 			# Add configuration group
 			Phoenx.add_groups_for_files(@project, @target_spec.config_files.values)
